cbea1b76e9e63f25957a10086449ea8b960885d8
msl <msl@msls-Virtual-Machine.local>
2026-06-19 13:59:29 +0200

    custom LOCAL dev testic .env setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: './',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    base: './',
+    server: {
+      host: true,
+      port: 5173,
+      allowedHosts: env.VITE_ALLOWED_HOSTS ? [env.VITE_ALLOWED_HOSTS] : [],
+    },
+  }
 })
